d997a28f7dd146a7dbadd53afdade6d0ccbab77e
hiby90hou <hiby.90hou@qq.com>
2020-06-08 21:36:19 +1000

    add item modal still inprogress

--- app/shopping-list-details/components/AddItem.tsx
+++ app/shopping-list-details/components/AddItem.tsx
@@ -5,7 +5,8 @@ import {
   StyleSheet,
   Text,
   TouchableHighlight,
-  View
+  View,
+  TextInput
 } from "react-native";
 
 type Props = {
@@ -23,10 +24,9 @@ const styles = StyleSheet.create({
     marginTop: 22
   },
   modalView: {
-    margin: 20,
     backgroundColor: "white",
     borderRadius: 20,
-    padding: 35,
+    overflow: "hidden",
     alignItems: "center",
     shadowColor: "#000",
     shadowOffset: {
@@ -53,17 +53,54 @@ const styles = StyleSheet.create({
     color: "white",
     fontWeight: "bold",
     fontSize: 70,
-    textAlign: "center",
+    textAlign: "left",
     marginBottom: 5
   },
-  modalText: {
-    marginBottom: 15,
-    textAlign: "center"
+  modalHeader: {
+    display: "flex",
+    flexDirection: "row",
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    width: "100%"
+  },
+  modalTitle: {
+    padding: 15,
+    textAlign: "center",
+    backgroundColor: '#fff',
+  },
+  modalCloseBtn: {
+    margin: 15,
+    width: 20,
+    height: 20,
+    backgroundColor: '#ccc',
+    borderRadius: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  modalBody: {
+    display: "flex",
+    flexDirection: "column",
+    margin: 15
+  },
+  inputBox: {
+    borderColor: 'gray',
+    borderWidth: 1
+  },
+  colorTag: {
+    backgroundColor: "#ccc",
+    margin: 3,
+    padding: 3
+  },
+  tagGroup: {
+    display: "flex",
+    flexDirection: "row",
   }
 });
 
-export function AddItem({}: Props) {
+export function AddItem({ }: Props) {
   const [modalVisible, setModalVisible] = useState(false);
+  const [itemName, setItemName] = useState('');
+  const [itemNote, setItemNote] = useState('');
   return (
     <View style={styles.buttonContainer}>
       <Modal
@@ -76,16 +113,48 @@ export function AddItem({}: Props) {
       >
         <View style={styles.centeredView}>
           <View style={styles.modalView}>
-            <Text style={styles.modalText}>Hello World!</Text>
+            <View style={styles.modalHeader}>
+              <Text style={styles.modalTitle}>Add new item</Text>
+              <TouchableHighlight
+                style={styles.modalCloseBtn}
+                onPress={() => {
+                  setModalVisible(!modalVisible);
+                }}
+              >
+                <Text>x</Text>
+              </TouchableHighlight>
+            </View>
 
-            <TouchableHighlight
-              style={{ backgroundColor: "#2196F3" }}
-              onPress={() => {
-                setModalVisible(!modalVisible);
-              }}
-            >
-              <Text>Hide Modal</Text>
-            </TouchableHighlight>
+            <View style={styles.modalBody}>
+              <Text>item name</Text>
+              <TextInput
+                style={styles.inputBox}
+                onChangeText={text => setItemName(text)}
+                value={itemName}
+              />
+              <Text>item tag (Limit to 3)</Text>
+              <View style={styles.tagGroup}>
+                <View style={styles.colorTag}>
+                  <Text>Easy to expire</Text>
+                </View>
+                <View style={styles.colorTag}>
+                  <Text>Fridge</Text>
+                </View>
+                <View style={styles.colorTag}>
+                  <Text>meat</Text>
+                </View>
+              </View>
+              <Text>notes</Text>
+              <TextInput
+                style={styles.inputBox}
+                editable
+                maxLength={40}
+                onChangeText={text => setItemNote(text)}
+                value={itemNote}
+              />
+            </View>
+
+
           </View>
         </View>
       </Modal>
